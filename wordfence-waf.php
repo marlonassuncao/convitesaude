30f8ce241a9a6e879a3ba4bcfbdf4d0f021b8b77
Marlon Assunçao <contato@nawe.com.br>
2020-01-07 14:24:01 -0300

    V1.0

---
+++ wordfence-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
+
+if (file_exists('/Users/marlonassuncao/code/convite/wp-content/plugins/wordfence/waf/bootstrap.php')) {
+	define("WFWAF_LOG_PATH", '/Users/marlonassuncao/code/convite/wp-content/wflogs/');
+	include_once '/Users/marlonassuncao/code/convite/wp-content/plugins/wordfence/waf/bootstrap.php';
+}
+?>
